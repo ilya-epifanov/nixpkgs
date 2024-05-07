5cf64ce6c9f45d213a906be4b4d570e4194de11a
Ilya Epifanov <mail@ilya.network>
2024-05-07 16:01:14 +0200

    betaflight-configurator: fixed app initialization

--- pkgs/applications/science/robotics/betaflight-configurator/default.nix
+++ pkgs/applications/science/robotics/betaflight-configurator/default.nix
@@ -1,27 +1,20 @@
-{lib, stdenv, fetchurl, unzip, makeDesktopItem, nwjs, wrapGAppsHook3, gsettings-desktop-schemas, gtk3 }:
+{lib, stdenv, fetchurl, unzip, makeDesktopItem, nwjs084, wrapGAppsHook3, gsettings-desktop-schemas, gtk3, version, sha256 }:
 
-let
-  pname = "betaflight-configurator";
-  desktopItem = makeDesktopItem {
-    name = pname;
-    exec = pname;
-    icon = pname;
-    comment = "Betaflight configuration tool";
-    desktopName = "Betaflight Configurator";
-    genericName = "Flight controller configuration tool";
-  };
-in
 stdenv.mkDerivation rec {
-  inherit pname;
-  version = "10.10.0";
+  pname = "betaflight-configurator";
+  inherit version;
+  shortVersion = lib.versions.majorMinor version;
+  pname_versioned = pname + "." + shortVersion;
+
   src = fetchurl {
     url = "https://github.com/betaflight/${pname}/releases/download/${version}/${pname}_${version}_linux64-portable.zip";
-    sha256 = "sha256-UB5Vr5wyCUZbOaQNckJQ1tAXwh8VSLNI1IgTiJzxV08=";
+    inherit sha256;
   };
 
   # remove large unneeded files
   postUnpack = ''
     find -name "lib*.so" -delete
+    find -name "lib*.so.*" -delete
   '';
 
   nativeBuildInputs = [ wrapGAppsHook3 unzip ];
@@ -31,19 +24,27 @@ stdenv.mkDerivation rec {
   installPhase = ''
     runHook preInstall
     mkdir -p $out/bin \
-             $out/opt/${pname}
+             $out/opt/${pname_versioned}
 
-    cp -r . $out/opt/${pname}/
-    install -m 444 -D icon/bf_icon_128.png $out/share/icons/hicolor/128x128/apps/${pname}.png
+    cp -r . $out/opt/${pname_versioned}/
+    install -m 444 -D icon/bf_icon_128.png $out/share/icons/hicolor/128x128/apps/${pname_versioned}.png
     cp -r ${desktopItem}/share/applications $out/share/
 
-    makeWrapper ${nwjs}/bin/nw $out/bin/${pname} --add-flags $out/opt/${pname}
+    makeWrapper ${nwjs084}/bin/nw $out/bin/${pname_versioned} --add-flags $out/opt/${pname_versioned}
     runHook postInstall
   '';
 
+  desktopItem = makeDesktopItem {
+    name = pname_versioned;
+    exec = pname_versioned;
+    icon = pname_versioned;
+    comment = "Betaflight configuration tool " + version;
+    desktopName = "Betaflight Configurator " + version;
+    genericName = "Flight controller configuration tool";
+  };
   meta = with lib; {
     description = "The Betaflight flight control system configuration tool";
-    mainProgram = "betaflight-configurator";
+    mainProgram = pname_versioned;
     longDescription = ''
       A crossplatform configuration tool for the Betaflight flight control system.
       Various types of aircraft are supported by the tool and by Betaflight, e.g.
@@ -52,7 +53,7 @@ stdenv.mkDerivation rec {
     homepage    = "https://github.com/betaflight/betaflight/wiki";
     sourceProvenance = with sourceTypes; [ binaryNativeCode ];
     license     = licenses.gpl3;
-    maintainers = with maintainers; [ wucke13 ];
+    maintainers = with maintainers; [ wucke13 ilya-epifanov ];
     platforms   = platforms.linux;
   };
 }
